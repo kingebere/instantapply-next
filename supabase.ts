22a58a3d1c077a18988781ad912389453987e8a9
kingebere <gudoski30@gmail.com>
2023-04-13 23:10:00 +0100

    feat:first commit

---
+++ supabase.ts
@@ -0,0 +1,31 @@
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL ||"";
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY||"";
+export const supabase = createClient(supabaseUrl, supabaseKey);
+
+
+
+//Google Oauth
+export async function signInWithGoogle() {
+	let redirectUrl =
+		process.env.NODE_ENV === 'production'
+			? 'https://google.com'
+			: 'http://localhost:3000/';
+
+	const { data, error } = await supabase.auth.signInWithOAuth({
+		provider: 'google',
+		options: {
+			redirectTo: redirectUrl,
+		},
+	});
+	return data;
+}
+
+//supabase session
+export async function getSession() {
+	const {
+		data: { session },
+	} = await supabase.auth.getSession();
+	return session;
+}
