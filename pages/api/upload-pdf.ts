b0198b223e948c7a7ffc8daf6fa874c56d2d2ed7
kingebere <gudoski30@gmail.com>
2023-04-14 01:24:29 +0100

    feat:store data in cloudfare

---
+++ pages/api/upload-pdf.ts
@@ -0,0 +1,42 @@
+// Require the library
+
+
+import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import type { NextApiRequest, NextApiResponse } from 'next';
+
+
+
+
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
+console.log("great",req.body)
+
+const insertToProfile = async () => {
+const client = new S3Client({
+   region:"auto",   
+   endpoint: `https://${process.env.NEXT_PUBLIC_S3_ACCOUNT_ID}.r2.cloudflarestorage.com`,
+   credentials:{
+    accessKeyId: `${process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID}`,
+secretAccessKey: `${process.env.NEXT_PUBLIC_S3_ACCESS_KEY_SECRET}`,
+
+   }
+});
+
+  const command = new PutObjectCommand({
+    Bucket: "test-bucket",
+    Key: "hello-s3.txt",
+    Body: "Hello S3!",
+  });
+
+  try {
+    const response = await client.send(command);
+    console.log(response);
+  } catch (err) {
+    console.error(err);
+  }
+insertToProfile()
+}
+
+}
+
+
+
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link';
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import {signInWithGoogle} from "../supabase"
@@ -45,8 +46,11 @@ export default function Home() {
                         <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
                     </li>
                     <li>
-                        <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Features</a>
-                    </li>
+                      <Link href="/onboarding">
+                      <h3  className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Onboarding</h3>
+                  
+                      </Link>
+                   </li>
                   
                     <li>
                         <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 lg:p-0 dark:text-gray-400 lg:dark:hover:text-white dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
