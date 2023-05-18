841eb952004c5ad8466b3cc37bf2a69d9589e908
codewarsfx <chiderainnocent001@gmail.com>
2023-05-18 23:24:46 +0100

    context auth

---
+++ context/AuthContext.tsx
@@ -0,0 +1,15 @@
+import { User } from '@supabase/supabase-js'
+
+import React from 'react'
+import { createContext } from 'vm'
+
+
+const AuthContext = 
+
+
+export default function AuthProvider() {
+
+  return (
+    <div>AuthContext</div>
+  )
+}
