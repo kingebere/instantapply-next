3fb970e371cb9f6ef8257c74a2a03d6dca43d2f7
kingebere <gudoski30@gmail.com>
2023-06-04 14:48:39 +0100

    feat:make pdf show properly

--- pages/api/upload-pdf.ts
+++ pages/api/upload-pdf.ts
@@ -2,11 +2,6 @@ import S3 from "aws-sdk/clients/s3.js"
 
 import type { NextApiRequest, NextApiResponse } from "next"
 
-// export const config = {
-//   api: {
-//     bodyParser: false
-//   }
-// };
 
 const s3 = new S3({
   endpoint: `https://${process.env.NEXT_PUBLIC_S3_ACCOUNT_ID}.r2.cloudflarestorage.com`,
@@ -17,78 +12,39 @@ const s3 = new S3({
   // region:'auto',
 })
 
-export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
-) {
-  // const name = req.body.name
-  // const file = req.body.file
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  try {
+    // Extract the base64-encoded PDF data from the request body
+    const { pdfBlob } = req.body;
 
- 
-  const params = {
-    Bucket: "instantapply",
-    Key: req.query.file as string,
-    Body: req.body,
-    ContentType: req.query.fileType as string,
+    // Convert the base64 data to a Buffer object
+    const base64Data = Buffer.from(pdfBlob.replace(/^data:application\/\w+;base64,/, ""), "base64");
+
+    // Define the parameters for uploading to S3
+    const params = {
+      Bucket: "instantapply",
+      Key: req.query.file as string,
+      Body: base64Data,
+      ContentType: req.query.fileType as string,
+    };
+
+    // Upload the file to S3
+    s3.upload(params, function (err: any, data: any) {
+      if (err) {
+        // If an error occurs during the upload, log the error and send a response with an error message
+        console.log(err, err.stack);
+        return res.status(500).json({ error: 'Failed to upload file' });
+      } else {
+        // If the upload is successful, send a response with the URL of the uploaded file -- for signed url
+        // If the upload is successful.send a response with the public url and the data key  -- for public url
+        return res.json({ url:`https://pub-1b721db15d0342048e62b290ea6ae12a.r2.dev/${data.Key}`  });
+      }
+    });
+  } catch (error) {
+    // Handle any other errors that may occur within the try block
+    console.error('An error occurred:', error);
+    return res.status(500).json({ error: 'Failed to upload file' });
   }
-  s3.upload(params, function (err: any, data: any) {
-    if (err) console.log(err, err.stack) // an error occurred
-    else return res.json({ url: data.Location }) // successful response
-  })
 }
 
-// import S3 from 'aws-sdk/clients/s3.js';
 
-// import type { NextApiRequest, NextApiResponse } from 'next';
-
-// import formidable from 'formidable';
-// import fs from 'fs';
-// export const config = {
-//   api: {
-//     bodyParser: false,
-//   },
-// };
-
-// export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-
-// const s3 = new S3({
-//     endpoint: `https://${process.env.NEXT_PUBLIC_S3_ACCOUNT_ID}.r2.cloudflarestorage.com`,
-//     accessKeyId: `${process.env.NEXT_PUBLIC_S3_ACCESS_KEY_ID}`,
-//     secretAccessKey: `${process.env.NEXT_PUBLIC_S3_ACCESS_KEY_SECRET}`,
-//     signatureVersion: "v4",
-//     s3ForcePathStyle: true
-//     // region:'auto',
-//   });
-//        // parse request to readable form
-//   const form = new formidable.IncomingForm();
-//   form.parse(req, async (err, fields, files) => {
-//     // Account for parsing errors
-//     if (err) return res.status(500);
-//     // Read file
-//     console.log("v",fields)
-//     console.log("l",files)
-//     const file = fs.readFileSync(files.file.path);
-//     // Upload the file
-//     s3.upload({
-//       // params
-//       Bucket: "instantapply",
-//       Key:"upload.png",
-//       Body:file,
-//       ContentType: "application/pdf",
-//     })
-//     .send((err, data) => {
-//       if (err) {
-//         console.log('err',err)
-//         return res.status(500);
-//       };
-//       if (data) {
-//         console.log('data',data)
-
-//         return res.json({
-//           url:  data.Location,
-//         });
-//       };
-//     });
-//   });
-
-// }
--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -1,189 +1,208 @@
-import useAuth from "@/hooks/useAuth";
-import { insertToProfile } from "@/supabase";
-import { useState } from "react";
+import useAuth from "@/hooks/useAuth"
+import { insertToProfile } from "@/supabase"
+import { useState } from "react"
 
 export default function Onboarding() {
-	const user = useAuth();
-	const [formData, setFormData] = useState({
-		resume_email: "",
-		firstname: "",
-		lastname: "",
-		resume_url: "",
-	});
+  const user = useAuth()
+  const [formData, setFormData] = useState({
+    resume_email: "",
+    firstname: "",
+    lastname: "",
+    resume_url: "",
+  })
 
+  async function handleChange(event: any) {
+    // Extract the name of the input field and the selected file
+    const fieldName = event.target.name
+    const file = event.target.files[0]
 
+    // Encode the file name and type using encodeURIComponent to ensure safe URL parameter values
+    const filename = encodeURIComponent(file.name)
+    const fileType = encodeURIComponent(file.type)
 
-	// CHECK THAT THE FILE IS PROPER FORMAT (size, type, etc)
+    // Function to convert Blob to base64
+    function toBase64(file: Blob) {
+      const reader = new FileReader()
+      return new Promise((resolve, reject) => {
+        reader.readAsDataURL(file)
+        reader.onload = function () {
+          resolve(reader.result) // Resolve with the base64 data
+        }
+        reader.onerror = reject
+      })
+    }
 
-	async function handleChange(event: any) {
-		console.log(event.target.files[0]);
-		const fieldName = event.target.name;
-		const file = event.target.files[0];
-		const formData = new FormData();
-		formData.append('file', file);
-		const filename = encodeURIComponent(file.name)
-		const fileType = encodeURIComponent(file.type)
-		const data = await fetch(`/api/upload-pdf?file=${filename}&fileType=${fileType}`, {
-			method: "POST",
-			mode: "cors",
-			body: formData,
-			headers: {
-				"Content-Type": "application/pdf",
-			},
-		});
-		const res = await data.json();
-		setFormData((prev) => ({
-			// Retain the existing values
-			...prev,
-			// update the current field
-			[fieldName]: res.url,
-		}));
-	}
+    // Convert the file to base64
+    const pdfBlob = await toBase64(file)
 
-	//add input values to the formdata state
-	function updateName(e: { target: { name: any; value: any } }) {
-		const fieldName = e.target.name;
-		setFormData((prev) => ({
-			// Retain the existing values
-			...prev,
-			// update the current field
-			[fieldName]: e.target.value,
-		}));
-	}
+    // Send a POST request to the server to upload the PDF file
+    const data = await fetch(
+      `/api/upload-pdf?file=${filename}&fileType=${fileType}`,
+      {
+        method: "POST",
+        mode: "cors",
+        body: JSON.stringify({ pdfBlob }), // Send the base64-encoded PDF data in the request body
+        headers: {
+          "Content-Type": "application/json",
+        },
+      }
+    )
 
-	//submit formdata
-	async function formSubmit(e: any) {
-		e.preventDefault();
-		const data = await insertToProfile(formData, user);
-	}
-	console.log(user);
-	return (
-		<>
-			<section className='bg-gray-50 dark:bg-gray-900'>
-				<div className='flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0'>
-					<a
-						href='#'
-						className='flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white'
-					>
-						<img
-							className='w-8 h-8 mr-2'
-							src='/assets/images/Instantapply-logo.png'
-							alt='logo'
-						/>
-						InstantApply
-					</a>
-					<div className='w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700'>
-						<div className='p-6 space-y-4 md:space-y-6 sm:p-8'>
-							<h1 className='text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white'>
-								Add Profile
-							</h1>
-							<form className='space-y-4 md:space-y-6' onSubmit={formSubmit}>
-								<div>
-									<label
-										htmlFor='email'
-										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-									>
-										Your email
-									</label>
-									<input
-										type='email'
-										name='resume_email'
-										id='email'
-										value={formData.resume_email}
-										onChange={updateName}
-										className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-										placeholder='name@company.com'
-										required
-									/>
-								</div>
-								<div>
-									<label
-										htmlFor='text'
-										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-									>
-										First Name
-									</label>
-									<input
-										type='text'
-										name='firstname'
-										id='firstname'
-										value={formData.firstname}
-										onChange={updateName}
-										placeholder='Dave King'
-										className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-										required
-									/>
-								</div>
-								<div>
-									<label
-										htmlFor='text'
-										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-									>
-										Last Name
-									</label>
-									<input
-										type='text'
-										name='lastname'
-										id='lastname'
-										value={formData.lastname}
-										onChange={updateName}
-										placeholder='Dave King'
-										className='bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-										required
-									/>
-								</div>
-								<div>
-									<label
-										htmlFor='file'
-										className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
-									>
-										Input CV
-									</label>
-									<input
-										onChange={handleChange}
-										accept='image/*,.pdf'
-										className='block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400'
-										id='default_size'
-										name='resume_url'
-										type='file'
-									/>
-								</div>
-								<div className='flex items-start'>
-									<div className='flex items-center h-5'>
-										<input
-											id='terms'
-											aria-describedby='terms'
-											type='checkbox'
-											className='w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800'
-											required
-										/>
-									</div>
-									<div className='ml-3 text-sm'>
-										<label
-											htmlFor='terms'
-											className='font-light text-gray-500 dark:text-gray-300'
-										>
-											I accept the{" "}
-											<a
-												className='font-medium text-primary-600 hover:underline dark:text-primary-500'
-												href='#'
-											>
-												Terms and Conditions
-											</a>
-										</label>
-									</div>
-								</div>
-								<button
-									type='submit'
-									className='w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800'
-								>
-									Create a profile
-								</button>
-							</form>
-						</div>
-					</div>
-				</div>
-			</section>
-		</>
-	);
+    // Parse the response data as JSON
+    const res = await data.json()
+
+    setFormData((prev) => ({
+      // Retain the existing values
+      ...prev,
+      // update the current field
+      [fieldName]: res.url,
+    }))
+  }
+
+  //add input values to the formdata state
+  function updateName(e: { target: { name: any; value: any } }) {
+    const fieldName = e.target.name
+    setFormData((prev) => ({
+      // Retain the existing values
+      ...prev,
+      // update the current field
+      [fieldName]: e.target.value,
+    }))
+  }
+
+  //submit formdata
+  async function formSubmit(e: any) {
+    e.preventDefault()
+    const data = await insertToProfile(formData, user)
+  }
+
+  return (
+    <>
+      <section className="bg-gray-50 dark:bg-gray-900">
+        <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+          <a
+            href="#"
+            className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white"
+          >
+            <img
+              className="w-8 h-8 mr-2"
+              src="/assets/images/Instantapply-logo.png"
+              alt="logo"
+            />
+            InstantApply
+          </a>
+          <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+            <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+                Add Profile
+              </h1>
+              <form className="space-y-4 md:space-y-6" onSubmit={formSubmit}>
+                <div>
+                  <label
+                    htmlFor="email"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Your email
+                  </label>
+                  <input
+                    type="email"
+                    name="resume_email"
+                    id="email"
+                    value={formData.resume_email}
+                    onChange={updateName}
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="name@company.com"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    First Name
+                  </label>
+                  <input
+                    type="text"
+                    name="firstname"
+                    id="firstname"
+                    value={formData.firstname}
+                    onChange={updateName}
+                    placeholder="Dave King"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Last Name
+                  </label>
+                  <input
+                    type="text"
+                    name="lastname"
+                    id="lastname"
+                    value={formData.lastname}
+                    onChange={updateName}
+                    placeholder="Dave King"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+                <div>
+                  <label
+                    htmlFor="file"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Input CV
+                  </label>
+                  <input
+                    onChange={handleChange}
+                    accept="image/*,.pdf"
+                    className="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                    id="default_size"
+                    name="resume_url"
+                    type="file"
+                  />
+                </div>
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input
+                      id="terms"
+                      aria-describedby="terms"
+                      type="checkbox"
+                      className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                      required
+                    />
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label
+                      htmlFor="terms"
+                      className="font-light text-gray-500 dark:text-gray-300"
+                    >
+                      I accept the{" "}
+                      <a
+                        className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                        href="#"
+                      >
+                        Terms and Conditions
+                      </a>
+                    </label>
+                  </div>
+                </div>
+                <button
+                  type="submit"
+                  className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+                >
+                  Create a profile
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </section>
+    </>
+  )
 }
