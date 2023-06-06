fe536a43ca1e96aecd298272cec57ff957b16f7e
kingebere <gudoski30@gmail.com>
2023-06-06 02:24:40 +0100

    feat:include gender and youlatino in form

--- pages/onboarding/index.tsx
+++ pages/onboarding/index.tsx
@@ -12,7 +12,9 @@ export default function Onboarding() {
     filename: "",
 	phone:"",
 	github:"",
-	linkedin:""
+	linkedin:"",
+	gender:"",
+	youlatino:"",
   })
 
   async function handleChange(event: any) {
@@ -202,6 +204,43 @@ export default function Onboarding() {
                   />
                 </div>
 
+				<div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Are you Latino?
+                  </label>
+                  <input
+                    type="text"
+                    name="youlatino"
+                    id="youlatino"
+                    value={formData.youlatino}
+                    onChange={updateName}
+                    placeholder="Dave King"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
+
+				<div>
+                  <label
+                    htmlFor="text"
+                    className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                  >
+                    Gender
+                  </label>
+                  <input
+                    type="text"
+                    name="gender"
+                    id="gender"
+                    value={formData.gender}
+                    onChange={updateName}
+                    placeholder="Dave King"
+                    className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    required
+                  />
+                </div>
 
 
 				<div>
