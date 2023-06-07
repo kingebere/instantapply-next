d10d645ddef39de88f0085c6e0dac0e9e1ac8ddf
kingebere <gudoski30@gmail.com>
2023-06-07 01:23:48 +0100

    fix:change logo

--- pages/index.tsx
+++ pages/index.tsx
@@ -78,7 +78,7 @@ export default function Home() {
         <div className="flex flex-wrap justify-between items-center mx-auto max-w-screen-xl">
             <a href="https://flowbite.com" className="flex items-center">
                 <img src="/assets/images/Instantapply-logo.png" className="mr-3 h-6 sm:h-9" alt="Flowbite Logo" />
-                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">InstantApply</span>
+                <span className="self-center text-xl font-semibold whitespace-nowrap dark:text-white">Instant</span>
             </a>
             <div className="flex items-center lg:order-2">
                 <a href="#" className="text-gray-800 dark:text-white hover:bg-gray-50 focus:ring-4 focus:ring-gray-300 font-medium rounded-lg text-sm px-4 lg:px-5 py-2 lg:py-2.5 mr-2 dark:hover:bg-gray-700 focus:outline-none dark:focus:ring-gray-800">Log in</a>
