39961a9b57a7b905f306886b11c7ae6e4c4a7d17
kingebere <gudoski30@gmail.com>
2023-06-14 10:07:32 +0100

    fix:remove image acceptance in file upload

--- components/FormStepper/index.tsx
+++ components/FormStepper/index.tsx
@@ -219,7 +219,7 @@ export default function FormStepper({
                     </label>
                     <input
                       onChange={handleChange}
-                      accept="image/*,.pdf"
+                      accept=".pdf"
                       className="block w-full mb-5 text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
                       id="default_size"
                       name="resume_url"
