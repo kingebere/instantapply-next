f5811c59f645b56fa8fc302733b40a91ad264f4c
kingebere <gudoski30@gmail.com>
2023-06-25 13:39:32 +0100

    feat:remove section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { useEffect } from "react";
+
 import Image from "next/image";
 import Link from "next/link";
 import { Inter } from "next/font/google";
@@ -16,20 +16,6 @@ export default function Home() {
     `https://instantapply.co` + (router.asPath === "/" ? "" : router.asPath)
   ).split("?")[0];
 
-  useEffect(() => {
-    async function getParsedData() {
-      // Send a GET request to the server to get the parsed PDF file
-      const response = await fetch(`/api/pdfParser`, {
-        method: "GET",
-        mode: "cors",
-      });
-
-      // Parse the response data as JSON
-      const res = await response.json();
-      console.log(res, "res");
-    }
-    getParsedData();
-  }, []);
   return (
     <>
       <Head>
@@ -264,39 +250,6 @@ export default function Home() {
               height: "100%",
             }}
           ></iframe>
-        </div>
-      </section>
-
-      <section className="bg-white dark:bg-gray-900 mb-10 ">
-        <div className="gap-8 items-center overflow-hidden py-8 h-screen bg-gradient-bitwhite relative px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:pt-2 sm:pb-8 lg:px-6">
-          <div className={styles.rod1}></div>
-          <div className={styles.rod2}></div>
-          <div className={styles.rod3}></div>
-          <div className="w-full  relative h-96"></div>
-          <div className="mt-4 md:mt-0">
-            <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-              Get Hired Faster when you apply faster
-            </h2>
-            {/* <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">Flowbite helps you connect with friends and communities of people who share your interests. Connecting with your friends and family as well as discovering new ones is easy with features like Groups.</p> */}
-            <a
-              href="#"
-              className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
-            >
-              Download Chrome Extension
-              <svg
-                className="ml-2 -mr-1 w-5 h-5"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                  clipRule="evenodd"
-                ></path>
-              </svg>
-            </a>
-          </div>
         </div>
       </section>
     </>
--- supabase.ts
+++ supabase.ts
@@ -1,8 +1,5 @@
 import { Session, createClient } from "@supabase/supabase-js";
 
-
-
-
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
 export const supabase = createClient(supabaseUrl, supabaseKey);
