ecac43fad56898944ad4e6e89692187e12d97d25
kingebere <gudoski30@gmail.com>
2023-06-26 18:20:11 +0100

    feat:settimeout for websockets

--- pages/api/tracking.gif.ts
+++ pages/api/tracking.gif.ts
@@ -11,34 +11,49 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     // Send a message to the WebSocket server
     const message = { email: email, count: 1 };
 
-    try {
-      // Connect to the WebSocket server
+    const connectWebSocket = () => {
       const socket = new WebSocket("wss://instantapplywebsockett.onrender.com");
 
-       // Wait for the WebSocket connection to be established
-    socket.on('open', () => {
-      console.log('WebSocket connection established',message);
+      // Wait for the WebSocket connection to be established
+      socket.on('open', () => {
+        console.log('WebSocket connection established', message);
 
-      // Send a message to the WebSocket server
+        // Send a message to the WebSocket server
+        socket.send(JSON.stringify(message));
+      });
 
-      socket.send(JSON.stringify(message));
-    });
+      // Handle WebSocket messages received from the server
+      socket.on('message', (message) => {
+        console.log('Received message from WebSocket server:', message);
 
-     // Handle WebSocket messages received from the server
-     socket.on('message', (message) => {
-      console.log('Received message from WebSocket server:', message);
+        // Close the WebSocket connection
+        socket.close();
+      });
 
-      // Close the WebSocket connection
-      socket.close();
-    });
+      // Handle WebSocket connection close
+      socket.on('close', () => {
+        console.log('WebSocket connection closed');
 
-    // Handle WebSocket connection close
-    socket.on('close', () => {
-      console.log('WebSocket connection closed');
+        // Retry connection after a delay
+        setTimeout(connectWebSocket, 5000); // Retry after 5 seconds
+      });
 
-    });
-    // Return a success response
-    res.status(200).json({ success: true });
+      // Handle WebSocket errors
+      socket.on('error', (error) => {
+        console.error('WebSocket error:', error);
+
+        // Close the WebSocket connection and retry after a delay
+        socket.close();
+        setTimeout(connectWebSocket, 5000); // Retry after 5 seconds
+      });
+    };
+
+    try {
+      // Start the WebSocket connection
+      connectWebSocket();
+
+      // Return a success response
+      res.status(200).json({ success: true });
     } catch (error) {
       // Handle errors
       console.error("Error sending notification:", error);
