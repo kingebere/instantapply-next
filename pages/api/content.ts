13ca4c95b05345c0b179189b4607007ab11e2ac6
codewarsfx <chiderainnocent001@gmail.com>
2023-06-26 15:11:27 +0100

    refactor:parsing and ai generation code

--- pages/api/content.ts
+++ pages/api/content.ts
@@ -3,83 +3,62 @@ import { supabase } from "../../supabase";
 import { getChatGPTResponse, parsePDFBYURL } from "@/lib/helpers";
 
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-  const allowedOrigin = [
-    "https://jobs.lever.co",
-    "https://boards.greenhouse.io",
-    "https://jobs.ashbyhq.com",
-    "https://mail.google.com",
-    "https://*.bamboohr.com",
-    "https://*.bamboohr.co.uk",
-  ];
-console.log("origin",req.headers.origin)
-  //since Access-Control-Allow-Origin doesnt allow multiple value , we
-  //make a checker that adds the allowed url based on the headers.origin
+	const allowedOrigin = [
+		"https://jobs.lever.co",
+		"https://boards.greenhouse.io",
+		"https://jobs.ashbyhq.com",
+		"https://mail.google.com",
+		"https://*.bamboohr.com",
+		"https://*.bamboohr.co.uk",
+	];
+	console.log("origin", req.headers.origin);
+	//since Access-Control-Allow-Origin doesnt allow multiple value , we
+	//make a checker that adds the allowed url based on the headers.origin
 
-    res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
-  
-  res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-  res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+	res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
 
-  // Handle preflight requests
-  if (req.method === "OPTIONS") {
+	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
 
-      res.setHeader(
-        "Access-Control-Allow-Origin",
-        req.headers.origin as string
-      );
-    
-    res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "Authorization, Content-Type"
-    );
-    res.status(200).end();
-    return;
-  }
+	// Handle preflight requests
+	if (req.method === "OPTIONS") {
+		res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
 
-  if (req.method === "POST") {
-    const { session, jobDescription } = req.body;
-    const {
-      data: {
-        session: { user },
-      },
-    } = JSON.parse(session);
+		res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Authorization, Content-Type"
+		);
+		res.status(200).end();
+		return;
+	}
 
-    try {
-      const { data, error } = await supabase
-        .from("profile")
-        .select("resume_url")
-        .eq("id", user.id);
-      if (error) throw error;
+	if (req.method === "POST") {
+		const { pdfText, jobDescription } = req.body;
 
-      const { resume_url } = data[0];
-      if (resume_url) {
-        //parse the user resume
-        const pdfText = await parsePDFBYURL(resume_url);
-
-        //get a tailored response for the resume and job description
-        if (jobDescription && pdfText) {
-          const chatGPTresponse = await getChatGPTResponse(
-            jobDescription,
-            pdfText
-          );
-          if (chatGPTresponse) {
-            res.status(200).json({
-              message: chatGPTresponse,
-            });
-          } else {
-            res.status(404).json({ message: "not found" });
-          }
-        }
-      }
-    } catch (e) {
-      console.error(e);
-      res.status(500).json({
-        message: "internal server error",
-      });
-    }
-  }
+		try {
+			//get a tailored response for the resume and job description
+			if (jobDescription && pdfText) {
+				const chatGPTresponse = await getChatGPTResponse(
+					jobDescription,
+					pdfText
+				);
+				if (chatGPTresponse) {
+					res.status(200).json({
+						message: chatGPTresponse,
+					});
+				} else {
+					res.status(404).json({ message: "not found" });
+				}
+			}
+		} catch (e) {
+			console.error(e);
+			res.status(500).json({
+				message: "internal server error",
+			});
+		}
+	}
 }
