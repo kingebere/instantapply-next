546aa8003eb2680aa358846c480e23712eed16ac
kingebere <gudoski30@gmail.com>
2023-06-28 17:27:09 +0100

    feat:add chrome extension url

--- pages/index.tsx
+++ pages/index.tsx
@@ -109,7 +109,7 @@ export default function Home() {
               </svg>
             </div>
             <a
-              href="#"
+              href="https://chrome.google.com/webstore/detail/instant-apply/ihgmeifeofnbegocgfecndeaohbigpon"
               className="inline-flex justify-center font-silka items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
             >
               <svg
