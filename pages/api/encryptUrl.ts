d39aac869464709c10f03a9ff46577af748cfc4c
kingebere <gudoski30@gmail.com>
2023-07-04 19:37:28 +0100

    feat:add headers

--- pages/api/encryptUrl.ts
+++ pages/api/encryptUrl.ts
@@ -13,8 +13,36 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
+    const allowedOrigin = [
+        "https://jobs.lever.co",
+        "https://boards.greenhouse.io",
+        "https://jobs.ashbyhq.com",
+        "https://mail.google.com",
+        "https://*.bamboohr.com",
+        "https://*.bamboohr.co.uk",
+      ];
+    
+      //since Access-Control-Allow-Origin doesnt allow multiple value , we
+      //make a checker that adds the allowed url based on the headers.origin
+      if (allowedOrigin.includes(req.headers.origin as string)) {
+        res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
+      }
+      res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+      res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+      // Handle preflight requests
+      if (req.method === "OPTIONS") {
+        res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
+    
+        res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+        res.setHeader(
+          "Access-Control-Allow-Headers",
+          "Authorization, Content-Type"
+        );
+        res.status(200).end();
+        return;
+      }
     const {match} =req.body
-
+console.log("match",match)
     const encryptedText = encrypt(match, secretKey);
 console.log('Encrypted:', encryptedText);
 res.status(200).send(encryptedText)
