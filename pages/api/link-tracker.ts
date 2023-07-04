ca94fc056e04caf5fdd98039991f866528666b94
kingebere <gudoski30@gmail.com>
2023-07-04 14:51:51 +0100

    feat:add decrypting logic

--- pages/api/link-tracker.ts
+++ pages/api/link-tracker.ts
@@ -1,11 +1,11 @@
 import { NextApiRequest, NextApiResponse } from "next";
-
+var forge = require('node-forge');
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
   try {
-    const { id, originalUrl } = req.query;
+    const { id } = req.query;
 
     // Increment the count or perform any tracking logic here
 
@@ -19,12 +19,29 @@ export default async function handler(
     // Log the tracking information
     console.log("Tracking Information:");
     console.log("ID:", id);
-    console.log("Original URL:", originalUrl);
+   
     console.log("IP Address:", ipAddress);
     console.log("Location:", location);
 
+    const secretKey = "Instant"; // Replace with your own secret key
+
+    // Convert the Base64-encoded encrypted string to bytes
+    const encryptedBytes = forge.util.decode64(id);
+
+    // Create a new decryption cipher
+    const decipher = forge.cipher.createDecipher("AES-CBC", secretKey);
+    decipher.start();
+    decipher.update(forge.util.createBuffer(encryptedBytes));
+    decipher.finish();
+
+    // Get the decrypted bytes
+    const decryptedBytes = decipher.output.getBytes();
+
+    // Convert the decrypted bytes to a UTF-8 string
+    const decryptedString = forge.util.decodeUtf8(decryptedBytes);
+console.log("decrypt",decryptedString)
     // Redirect to the original URL
-    res.writeHead(302, { Location: originalUrl });
+    res.writeHead(302, { Location: decryptedString });
     res.end();
   } catch (error) {
     console.error("Error:", error);
