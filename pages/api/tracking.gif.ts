f3431d1941bf60db525a993c2a13fcfb255e56b4
kingebere <gudoski30@gmail.com>
2023-07-05 01:32:48 +0100

    feat:readd websockets

--- pages/api/tracking.gif.ts
+++ pages/api/tracking.gif.ts
@@ -14,6 +14,10 @@ export default async function handler(
   if (req.method === "GET") {
     const { email, jobId, senderEmail } = req.query;
 
+
+    
+
+
     try {
       //only want this to happen if the user is the receiver
       if (jobId) {
@@ -28,6 +32,64 @@ export default async function handler(
         }
         //fecth the view count value for that job
         const viewCount = data[0]?.count;
+
+        const message = { email: email, count: 1 };
+
+        const connectWebSocket = () => {
+          const socket = new WebSocket("wss://instantapplywebsockett.onrender.com");
+        
+          // Wait for the WebSocket connection to be established
+          socket.on("open", () => {
+            console.log("WebSocket connection established", message);
+        
+            // Send a message to the WebSocket server
+            socket.send(JSON.stringify(message));
+          });
+        
+          // Handle WebSocket messages received from the server
+          socket.on("message", (message) => {
+            console.log("Received message from WebSocket server:", message);
+        
+            // Close the WebSocket connection
+            socket.close();
+          });
+        
+          // Handle WebSocket connection close
+          socket.on("close", () => {
+            console.log("WebSocket connection closed");
+        
+            // Retry connection after a delay
+            setTimeout(connectWebSocket, 5000); // Retry after 5 seconds
+          });
+        
+          // Handle WebSocket errors
+          socket.on("error", (error) => {
+            console.error("WebSocket error:", error);
+        
+            // Close the WebSocket connection and retry after a delay
+            socket.close();
+            setTimeout(connectWebSocket, 5000); // Retry after 5 seconds
+          });
+        };
+        
+        try {
+          // Start the WebSocket connection
+          connectWebSocket();
+        
+          // Return a success response
+          res.status(200).json({ success: true });
+        } catch (error) {
+          // Handle errors
+          console.error("Error sending notification:", error);
+        
+          // Return an error response
+          res.status(500).json({ error: "Failed to send notification" });
+          // }
+        } 
+        
+
+
+
         //if the count value is less that 0 send the message and update the count
         if (viewCount <= 0) {
           const sendEmailCommand = createSendEmailCommand(
