ec7a41db2710bdffd95606c65b988b7b1ead78b4
codewarsfx <chiderainnocent001@gmail.com>
2023-07-06 00:27:37 +0100

    feat: adding original url record to supabase

--- pages/api/encryptUrl.ts
+++ pages/api/encryptUrl.ts
@@ -1,116 +1,79 @@
-
 import { NextApiRequest, NextApiResponse } from "next";
-var crypto = require('crypto');
-import {nanoid} from "nanoid"
+import { supabase } from "@/supabase";
 
-function encrypt(text: any, secretKey: any) {
-    const cipher = crypto.createCipheriv('aes-256-cbc', secretKey, crypto.randomBytes(16));
-    let encrypted = cipher.update(text, 'utf8', 'hex');
-    encrypted += cipher.final('hex');
-    return encrypted;
-  }
-  // Function to generate a 256-bit key from a password (you can use a better key derivation function in production)
-function generateKey(password: string) {
-    return crypto
-      .createHash("sha256")
-      .update(password)
-      .digest();
-  }
-  const secretKey = generateKey("Instant");
 export default async function handler(
-  req: NextApiRequest,
-  res: NextApiResponse
+	req: NextApiRequest,
+	res: NextApiResponse
 ) {
-    const allowedOrigin = [
-        "https://jobs.lever.co",
-        "https://boards.greenhouse.io",
-        "https://jobs.ashbyhq.com",
-        "https://mail.google.com",
-        "https://*.bamboohr.com",
-        "https://*.bamboohr.co.uk",
-      ];
-    
-      //since Access-Control-Allow-Origin doesnt allow multiple value , we
-      //make a checker that adds the allowed url based on the headers.origin
-      if (allowedOrigin.includes(req.headers.origin as string)) {
-        res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
-      }
-      res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-      res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
-      // Handle preflight requests
-      if (req.method === "OPTIONS") {
-        res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
-    
-        res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
-        res.setHeader(
-          "Access-Control-Allow-Headers",
-          "Authorization, Content-Type"
-        );
-        res.status(200).end();
-        return;
-      }
-    const {match} =req.body
-     const shorten=nanoid(6)
-console.log(shorten); // outputs the encrypted data
-console.log('Encrypted:', shorten);
-res.status(200).send(shorten)
+	const allowedOrigin = [
+		"https://jobs.lever.co",
+		"https://boards.greenhouse.io",
+		"https://jobs.ashbyhq.com",
+		"https://mail.google.com",
+		"https://*.bamboohr.com",
+		"https://*.bamboohr.co.uk",
+	];
 
+	//since Access-Control-Allow-Origin doesnt allow multiple value , we
+	//make a checker that adds the allowed url based on the headers.origin
+	if (allowedOrigin.includes(req.headers.origin as string)) {
+		res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
+	}
+	res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+	res.setHeader("Access-Control-Allow-Headers", "Authorization, Content-Type");
+	// Handle preflight requests
+	if (req.method === "OPTIONS") {
+		res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
+
+		res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
+		res.setHeader(
+			"Access-Control-Allow-Headers",
+			"Authorization, Content-Type"
+		);
+		res.status(200).end();
+		return;
+	}
+	const { match, session } = req.body;
+
+	const {
+		data: {
+			session: { user },
+		},
+	} = JSON.parse(session);
+
+	//get match
+
+	if (match && user) {
+		try {
+			const { data, error } = await supabase
+				.from("superlinks")
+				.insert({
+					originalUrl: match,
+					user_id: user.id,
+				})
+				.select();
+
+			if (error) res.status(400).json({ message: "bad request" });
+			if (data) {
+				res.status(200).send(data[0]?.id);
+			}
+		} catch (error) {
+			console.log(error);
+			res.status(500).json({ message: "internal server error" });
+		}
+	} else {
+		res.status(400).json({ message: "bad request" });
+	}
+
+	//create a record for the match //record stores the original url
+
+	//return the id of the record
+
+	//      const shorten=nanoid(6)
+	// console.log(shorten); // outputs the encrypted data
+	// console.log('Encrypted:', shorten);
+	// res.status(200).send(shorten)
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // import { NextApiRequest, NextApiResponse } from "next";
 // var crypto = require('crypto');
@@ -142,7 +105,7 @@ res.status(200).send(shorten)
 //         "https://*.bamboohr.com",
 //         "https://*.bamboohr.co.uk",
 //       ];
-    
+
 //       //since Access-Control-Allow-Origin doesnt allow multiple value , we
 //       //make a checker that adds the allowed url based on the headers.origin
 //       if (allowedOrigin.includes(req.headers.origin as string)) {
@@ -153,7 +116,7 @@ res.status(200).send(shorten)
 //       // Handle preflight requests
 //       if (req.method === "OPTIONS") {
 //         res.setHeader("Access-Control-Allow-Origin", req.headers.origin as string);
-    
+
 //         res.setHeader("Access-Control-Allow-Methods", "GET, OPTIONS");
 //         res.setHeader(
 //           "Access-Control-Allow-Headers",
