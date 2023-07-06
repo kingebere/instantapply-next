1ba5b12012e815a66fd655a0d8add09a0adce3a5
codewarsfx <chiderainnocent001@gmail.com>
2023-07-06 01:16:23 +0100

    feat:ability to keep track of how many times a link is visited

--- pages/api/t.ts
+++ pages/api/t.ts
@@ -1,26 +1,6 @@
 import { supabase } from "@/supabase";
 import { NextApiRequest, NextApiResponse } from "next";
-var crypto = require("crypto");
 
-// // Decryption function
-// function decrypt(encryptedText: string | string[] | undefined, secretKey: any) {
-//     const algorithm = 'aes-256-cbc';
-//     const key = crypto.createHash('sha256').update(secretKey).digest();
-//     const iv = Buffer.alloc(16, 0); // Use a proper initialization vector (IV) for security
-
-//     const decipher = crypto.createDecipheriv(algorithm, key, iv);
-//     let decrypted = decipher.update(encryptedText, 'hex', 'utf8');
-//     decrypted += decipher.final('utf8');
-//     return decrypted;
-//   }
-// Function to generate a 256-bit key from a password (you can use a better key derivation function in production)
-//  function generateKey(password: string) {
-//     return crypto
-//       .createHash("sha256")
-//       .update(password)
-//       .digest();
-//   }
-// const secretKey = generateKey("Instant"); // Replace with your own secret key
 export default async function handler(
 	req: NextApiRequest,
 	res: NextApiResponse
@@ -69,20 +49,4 @@ export default async function handler(
 
 	// Get the user's location based on the country code
 	const location = req.headers["x-vercel-ip-country"];
-
-	// Log the tracking information
-
-	//     const algorithm = 'aes-256-cbc';
-	// const password = 'Instant';
-	// const key = crypto.scryptSync(password, 'salt', 32);
-	// const iv = Buffer.alloc(16, 0);
-
-	//     const decipher = crypto.createDecipheriv(algorithm, key, iv);
-	//     let decrypted = decipher.update(id, 'hex', 'utf8');
-	//     decrypted += decipher.final('utf8');
-	//     console.log(decrypted); // outputs the decrypted data
-
-	//     // Redirect to the original URL
-	//     res.writeHead(302, { Location: decrypted });
-	//     res.end();
 }
